6aa3b38c49f371577c27bf765d8376a6815cb517
Park-min-hyoung <qkralswgud1324@naver.com>
2023-02-22 14:07:30 +0900

    feat: 데이터 fetch 중일 때 loading render

--- src/components/post/index.tsx
+++ src/components/post/index.tsx
@@ -7,25 +7,29 @@ function PostList() {
 
   return (
     <>
-      {data?.map((post) => (
-        <div
-          style={{
-            width: '130px',
-            height: '130px',
-            backgroundColor: 'yellow',
-            marginBottom: '10px',
-          }}
-        >
-          <div>사진 url : {post.music.thumbnailUrl}</div>
-          <div>
-            {post.music.singer} - {post.music.musicName}
+      {isLoading ? (
+        <div>로딩 중 입니다~</div>
+      ) : (
+        data?.map((post) => (
+          <div
+            style={{
+              width: '130px',
+              height: '130px',
+              backgroundColor: 'yellow',
+              marginBottom: '10px',
+            }}
+          >
+            <div>사진 url : {post.music.thumbnailUrl}</div>
+            <div>
+              {post.music.singer} - {post.music.musicName}
+            </div>
+            <div>좋아요 {post.likeCount}</div>
+            <div>{post.isBattlePossible && '대결 가능'}</div>
+            <div>닉네임 {post.nickname}</div>
+            <div>장르 {post.music.genre}</div>
           </div>
-          <div>좋아요 {post.likeCount}</div>
-          <div>{post.isBattlePossible && '대결 가능'}</div>
-          <div>닉네임 {post.nickname}</div>
-          <div>장르 {post.music.genre}</div>
-        </div>
-      ))}
+        ))
+      )}
     </>
   );
 }
