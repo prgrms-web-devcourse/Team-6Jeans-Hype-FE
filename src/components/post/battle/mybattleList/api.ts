e59b345d6e788dcb14dd60da08b2ad21776f1834
Park-min-hyoung <qkralswgud1324@naver.com>
2023-02-23 18:36:07 +0900

    feat: 대결 신청 시 장르를 고려해 리스트 render

---
+++ src/components/post/battle/mybattleList/api.ts
@@ -0,0 +1,157 @@
+const DUMMY_DATA = [
+  [
+    {
+      success: true,
+      message: '대결 후보곡 리스트 조회 성공',
+      data: {
+        posts: [
+          {
+            postId: 1,
+            music: {
+              musicName: 'ditto',
+              thumbnailUrl: 'url',
+              singer: '뉴진스-발라드',
+            },
+          },
+        ],
+      },
+    },
+    {
+      success: true,
+      message: '대결 후보곡 리스트 조회 성공',
+      data: {
+        posts: [
+          {
+            postId: 2,
+            music: {
+              musicName: 'ditto',
+              thumbnailUrl: 'url',
+              singer: '뉴진스-발라드',
+            },
+          },
+        ],
+      },
+    },
+  ],
+  [
+    {
+      success: true,
+      message: '대결 후보곡 리스트 조회 성공',
+      data: {
+        posts: [
+          {
+            postId: 3,
+            music: {
+              musicName: 'ditto',
+              thumbnailUrl: 'url',
+              singer: '뉴진스-힙합',
+            },
+          },
+        ],
+      },
+    },
+    {
+      success: true,
+      message: '대결 후보곡 리스트 조회 성공',
+      data: {
+        posts: [
+          {
+            postId: 4,
+            music: {
+              musicName: 'ditto',
+              thumbnailUrl: 'url',
+              singer: '뉴진스-힙합',
+            },
+          },
+        ],
+      },
+    },
+  ],
+  [
+    {
+      success: true,
+      message: '대결 후보곡 리스트 조회 성공',
+      data: {
+        posts: [
+          {
+            postId: 5,
+            music: {
+              musicName: 'ditto',
+              thumbnailUrl: 'url',
+              singer: '뉴진스-랩',
+            },
+          },
+        ],
+      },
+    },
+    {
+      success: true,
+      message: '대결 후보곡 리스트 조회 성공',
+      data: {
+        posts: [
+          {
+            postId: 6,
+            music: {
+              musicName: 'ditto',
+              thumbnailUrl: 'url',
+              singer: '뉴진스-랩',
+            },
+          },
+        ],
+      },
+    },
+  ],
+  [
+    {
+      success: true,
+      message: '대결 후보곡 리스트 조회 성공',
+      data: {
+        posts: [
+          {
+            postId: 7,
+            music: {
+              musicName: 'ditto',
+              thumbnailUrl: 'url',
+              singer: '뉴진스-K-Pop',
+            },
+          },
+        ],
+      },
+    },
+    {
+      success: true,
+      message: '대결 후보곡 리스트 조회 성공',
+      data: {
+        posts: [
+          {
+            postId: 8,
+            music: {
+              musicName: 'ditto',
+              thumbnailUrl: 'url',
+              singer: '뉴진스-K-Pop',
+            },
+          },
+        ],
+      },
+    },
+  ],
+  [],
+];
+
+export const getMyBattleListData = async (genre: string) => {
+  try {
+    if (genre === '발라드') {
+      return DUMMY_DATA[0];
+    } else if (genre === '힙합') {
+      return DUMMY_DATA[1];
+    } else if (genre === '랩') {
+      return DUMMY_DATA[2];
+    } else if (genre === 'k-pop') {
+      return DUMMY_DATA[3];
+    } else if (genre === '재즈') {
+      return DUMMY_DATA[4];
+    }
+  } catch {
+    throw new Error('패칭 실패');
+  }
+};
--- src/components/post/battle/mybattleList/index.tsx
+++ src/components/post/battle/mybattleList/index.tsx
@@ -1,5 +1,37 @@
+import { useQuery } from '@tanstack/react-query';
+import { useRouter } from 'next/router';
+import { getMyBattleListData } from './api';
+
 function MyBattleList() {
-  return <div>배틀 리스트 입니다!</div>;
+  const router = useRouter();
+  const { genre } = router.query;
+
+  const { data: myBattleMusicList } = useQuery<any>(
+    ['post', 'battle', genre],
+    () => getMyBattleListData(genre as any),
+    {
+      enabled: !!genre,
+      select: (res) => {
+        return res.map((item: any) => item.data.posts[0]);
+      },
+    },
+  );
+
+  return (
+    <>
+      {myBattleMusicList?.length > 0 ? (
+        myBattleMusicList.map((list: any) => (
+          <div key={list.postId}>
+            <div>{list.music.musicName}</div>
+            <div>{list.music.thumbnailUrl}</div>
+            <div>{list.music.singer}</div>
+          </div>
+        ))
+      ) : (
+        <div>리스트가 없습니다</div>
+      )}
+    </>
+  );
 }
 
 export default MyBattleList;
