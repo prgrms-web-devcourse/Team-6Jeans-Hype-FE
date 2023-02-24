c01ed82cff83e19543097927f4e3536189f4f1a0
남주영 <njy1007@gmail.com>
2023-02-24 16:24:47 +0900

    feat: 로그인 페이지 마크업 (#27)
    * chore: svgr로 svg 컴포넌트화 할 수 있도록 설정
    
    * chore: 글로벌 스타일 설정
    
    * refactor: 폰트 비율 설정값 찾기 쉬운 위치로 옮기기
    
    * chore: 글로벌 width, height 설정
    
    * feat: 로그인 페이지 마크업

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,7 @@
 import type { AppProps } from 'next/app';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { Global } from '@emotion/react';
-import reset from '../styles/reset';
+import { global } from '@/styles/global';
 
 const queryClient = new QueryClient({
   defaultOptions: { queries: { refetchOnWindowFocus: false, retry: 1 } },
@@ -10,7 +10,7 @@ const queryClient = new QueryClient({
 export default function App({ Component, pageProps }: AppProps) {
   return (
     <QueryClientProvider client={queryClient}>
-      <Global styles={reset} />
+      <Global styles={global} />
       <Component {...pageProps} />
     </QueryClientProvider>
   );
