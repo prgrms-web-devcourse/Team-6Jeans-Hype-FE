5f308b975f9c4489e3889eced506c01873f41a12
shkim787\laptop <shkim061198@gmail.com>
2023-02-24 18:44:23 +0900

    feat: 디자인 추가

--- src/components/post/SearchMusics/MusicList.tsx
+++ src/components/post/SearchMusics/MusicList.tsx
@@ -8,78 +8,6 @@ import { getMusicData } from '@/utils/apis/music';
 
 import { Music } from './types';
 
-const Header = styled.div`
-  font-style: normal;
-  font-weight: 700;
-  font-size: 1.4rem;
-  line-height: 2rem;
-  padding-left: 0.8rem;
-`;
-
-const MusicListContainer = styled.div`
-  width: 100%;
-  display: flex;
-  flex-direction: column;
-  margin-top: 3rem;
-`;
-const MusicCardContainer = styled.div`
-  max-height: 300px;
-  overflow-y: scroll;
-`;
-const MusicCard = styled.div`
-  cursor: pointer;
-  display: flex;
-  align-items: center;
-  margin: 1rem 0;
-  padding: 0.3rem;
-  height: 7rem;
-  box-shadow: 0 0 1rem rgba(226, 226, 226, 0.25);
-  background-color: ${COLOR.white};
-  border-radius: 1rem;
-`;
-
-const ImageContainer = styled.div`
-  border-radius: 10px;
-  width: 6.4rem;
-  height: 6.4rem;
-  border: 0.1rem solid #dddddd;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-  & > img {
-    width: 5.5rem;
-    height: 5rem;
-  }
-`;
-
-const MusicTexts = styled.div`
-  width: calc(100% - 2rem - 7rem);
-  height: 60%;
-  cursor: pointer;
-  display: flex;
-  flex-direction: column;
-  margin-left: 2rem;
-`;
-
-const Text = styled.div`
-  height: 50%;
-  display: flex;
-  align-items: center;
-  line-height: 1.7rem;
-`;
-
-const Ellipsis = styled.div`
-  overflow: hidden;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  font-weight: bold;
-  & > span {
-    font-size: 1.2rem;
-  }
-`;
-const ArtistName = styled.div`
-  color: ${COLOR.gray};
-`;
 interface Props {
   onClickInMusicList(trackId: number): void;
   keyword: string;
@@ -135,10 +63,96 @@ function MusicList({ onClickInMusicList, keyword }: Props) {
             );
           })
         ) : (
-          <div>결과없음</div>
+          <EmptyResult>결과가 없습니다. 다른 검색어로 검색해보세요</EmptyResult>
         )}
       </MusicCardContainer>
     </MusicListContainer>
   );
 }
 export default memo(MusicList, (prev, next) => JSON.stringify(prev.keyword) === JSON.stringify(next.keyword));
+
+const Header = styled.div`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.4rem;
+  line-height: 2rem;
+  padding-left: 0.8rem;
+  margin-bottom: 1rem;
+`;
+
+const MusicListContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  margin-top: 3rem;
+`;
+
+const MusicCardContainer = styled.div`
+  height: calc(100vh - 15rem);
+  overflow-y: scroll;
+`;
+const MusicCard = styled.div`
+  cursor: pointer;
+  display: flex;
+  align-items: center;
+  margin: 1rem 0;
+  padding: 0.3rem;
+  height: 7rem;
+  box-shadow: 0 0 1rem rgba(226, 226, 226, 0.25);
+  background-color: ${COLOR.white};
+  border-radius: 1rem;
+`;
+
+const ImageContainer = styled.div`
+  border-radius: 10px;
+  width: 6.4rem;
+  height: 6.4rem;
+  border: 0.1rem solid #dddddd;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  & > img {
+    width: 5.5rem;
+    height: 5rem;
+  }
+`;
+
+const MusicTexts = styled.div`
+  width: calc(100% - 2rem - 7rem);
+  height: 60%;
+  cursor: pointer;
+  display: flex;
+  flex-direction: column;
+  margin-left: 2rem;
+`;
+
+const Text = styled.div`
+  height: 50%;
+  display: flex;
+  align-items: center;
+  line-height: 1.7rem;
+`;
+
+const Ellipsis = styled.div`
+  overflow: hidden;
+  text-overflow: ellipsis;
+  white-space: nowrap;
+  font-weight: bold;
+  & > span {
+    font-size: 1.2rem;
+  }
+`;
+
+const ArtistName = styled.div`
+  color: ${COLOR.gray};
+`;
+
+const EmptyResult = styled.div`
+  position: relative;
+  top: 50%;
+  text-align: center;
+  transform: translateY(-50%);
+  font-weight: 700;
+  font-size: 1.3rem;
+  color: ${COLOR.gray};
+`;
--- src/components/post/SearchMusics/SearchInput.tsx
+++ src/components/post/SearchMusics/SearchInput.tsx
@@ -2,50 +2,6 @@ import styled from '@emotion/styled';
 
 import { COLOR } from '@/constants/color';
 
-const SearchContainer = styled.div`
-  width: 100%;
-`;
-
-const TItle = styled.div`
-  font-style: normal;
-  font-weight: 700;
-  font-size: 1.4rem;
-  line-height: 2rem;
-  margin: 1rem 0;
-  padding-left: 0.8rem;
-`;
-
-const Searcher = styled.div`
-  width: 100%;
-  height: 35px;
-  display: flex;
-  align-items: center;
-  justify-content: space-between;
-  border-radius: 1rem;
-  background-color: ${COLOR.white};
-  box-shadow: 0 0 1.5rem rgba(158, 158, 158, 0.25);
-  border: 1px solid ${COLOR.blue};
-`;
-
-const Input = styled.input`
-  padding: 0 1rem;
-  width: calc(100% - 3rem);
-  border-radius: 1rem 0 0 1rem;
-  height: 100%;
-  color: ${COLOR.deepBlue};
-  font-size: 1.25rem;
-`;
-
-const SearcherButton = styled.button`
-  width: 2.5rem;
-  margin-right: 0.5rem;
-  height: 2.5rem;
-  border: 0;
-  & > img {
-    width: 1.1rem;
-  }
-`;
-
 interface Props {
   keyword: string;
   onChangeKeyword: React.ChangeEventHandler<HTMLInputElement | HTMLTextAreaElement>;
@@ -73,3 +29,49 @@ function SearchInput({ keyword, onChangeKeyword, onClickInSearchButton }: Props)
 }
 
 export default SearchInput;
+
+const SearchContainer = styled.div`
+  width: 100%;
+`;
+
+const TItle = styled.div`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.4rem;
+  line-height: 2rem;
+  margin: 1rem 0;
+  padding-left: 0.8rem;
+`;
+
+const Searcher = styled.div`
+  width: 100%;
+  height: 3.5rem;
+  display: flex;
+  align-items: center;
+  justify-content: center;
+  border-radius: 1rem;
+  background: linear-gradient(90deg, rgba(162, 116, 220, 1) -1.83%, rgba(101, 141, 244, 1) 86.44%);
+  box-shadow: 0 0 1.5rem rgba(158, 158, 158, 0.25);
+  border: 1px solid ${COLOR.blue};
+`;
+
+const Input = styled.input`
+  padding: 0 1rem;
+  width: calc(100% - 4.6rem);
+  border-radius: 0.9rem 0 0 0.9rem;
+  background-color: ${COLOR.white};
+  height: calc(100% - 0.2rem);
+  color: ${COLOR.deepBlue};
+  font-size: 1.25rem;
+`;
+
+const SearcherButton = styled.button`
+  width: 2.4rem;
+  border-radius: 0 0.9rem 0.9rem 0;
+  height: calc(100% - 0.2rem);
+  background-color: ${COLOR.white};
+  border: 0;
+  & > img {
+    width: 1.1rem;
+  }
+`;
