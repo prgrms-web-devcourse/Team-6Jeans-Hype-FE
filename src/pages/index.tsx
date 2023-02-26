3ed3de92a5f86a8cee821fe7eec17dfd7ba47013
ksh-jsx <shkim787@naver.com>
2023-02-26 23:36:32 +0900

    feat: text-divider 제작

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,4 @@
+import TextDivider from '@/components/common/TextDivider';
 import Link from 'next/link';
 
 export default function Home() {
@@ -6,6 +7,7 @@ export default function Home() {
       <Link href='/post/searchMusics' legacyBehavior>
         <a>create</a>
       </Link>
+      <TextDivider text1='내가바로음잘알' text2='2023-02-21' />
     </>
   );
 }
