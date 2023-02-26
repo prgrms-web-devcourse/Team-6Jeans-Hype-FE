0ec0db553f7449df95754d2b24a306c68216cc3b
Park-min-hyoung <qkralswgud1324@naver.com>
2023-02-26 16:07:16 +0900

    feat: 추천 글 상세 페이지 마크업

--- src/components/post/detail/index.tsx
+++ src/components/post/detail/index.tsx
@@ -3,6 +3,8 @@ import { getPostDetailData } from './api';
 import MusicInfo from './musicInfo';
 import { useRouter } from 'next/router';
 import { PostDetail } from './types';
+import styled from '@emotion/styled';
+import { COLOR } from '@/constants/color';
 
 function PostDetail() {
   const router = useRouter();
@@ -17,50 +19,250 @@ function PostDetail() {
   );
 
   return (
-    <>
-      <div>음악정보 컴포넌트</div>
+    <PostDetailContainer>
+      <PostDetailInfo>
+        <PrevButton src='/images/prev-button.png' />
 
-      {postDetail && (
-        <MusicInfo
-          musicName={postDetail.music.musicName}
-          musicUrl={postDetail.music.musicUrl}
-          thumbnailUrl={postDetail.music.thumbnailUrl}
-          singer={postDetail.music.singer}
-          genre={postDetail.music.genre}
-        />
-      )}
+        {postDetail && (
+          <MusicInfo
+            musicName={postDetail.music.musicName}
+            musicUrl={postDetail.music.musicUrl}
+            thumbnailUrl={postDetail.music.thumbnailUrl}
+            singer={postDetail.music.singer}
+            genre={postDetail.music.genre}
+          />
+        )}
 
-      <div>소개글 정보</div>
-      {postDetail?.content && (
-        <div
-          style={{
-            width: '400px',
-            height: '100px',
-            backgroundColor: 'yellow',
-            marginBottom: '10px',
-          }}
-        >
-          {postDetail?.content}
-        </div>
-      )}
+        <MusicPlayStatus>
+          <MusicPlayBar>
+            <div></div>
+            <div></div>
+            <div></div>
+          </MusicPlayBar>
+          <MusicPlayTime>
+            <div>1:46</div>
+            <div>4:10</div>
+          </MusicPlayTime>
+        </MusicPlayStatus>
 
-      <div>대결, 닉네임, 좋아요 컴포넌트</div>
-      <div
-        style={{
-          width: '400px',
-          height: '100px',
-          backgroundColor: 'yellow',
-          marginBottom: '10px',
-          display: 'flex',
-          justifyContent: 'space-between',
-        }}
-      >
-        {postDetail?.isBattlePossible && <div>대결 신청</div>}
-        <div>{postDetail?.nickname}</div>
-        <div>{postDetail?.likeCount}</div>
-      </div>
-    </>
+        <PostDetailEvent>
+          <PostIcon>
+            <img src='/images/white-full-heart.png' alt='좋아요 아이콘' />
+            <span>15</span>
+          </PostIcon>
+          <PostPlayIcon>
+            <audio src={postDetail?.music.musicUrl} controls loop></audio>
+          </PostPlayIcon>
+          <PostIcon>
+            <img src='/images/white-fire.png' alt='대결 아이콘' />
+            <span>15</span>
+          </PostIcon>
+        </PostDetailEvent>
+
+        {postDetail?.content && (
+          <PostDetailContent>
+            <ContenTitleContainer>
+              <ContentTitle>
+                <div></div>
+                <div>
+                  <div>{postDetail?.nickname}</div>
+                  <div>님의 한마디</div>
+                </div>
+                <div>
+                  <img src='/images/down-arrow.png' />
+                </div>
+              </ContentTitle>
+            </ContenTitleContainer>
+
+            <ContentContainer>
+              <Content>{postDetail.content}</Content>
+            </ContentContainer>
+          </PostDetailContent>
+        )}
+      </PostDetailInfo>
+    </PostDetailContainer>
   );
 }
 
 export default PostDetail;
+
+const PostDetailContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 100%;
+  height: 100%;
+  background-color: ${COLOR.purple};
+  position: relative;
+`;
+
+const PostDetailInfo = styled.div`
+  width: 90%;
+  margin: 2rem auto 0 auto;
+`;
+
+const PrevButton = styled.img`
+  width: 1.5rem;
+  height: 2rem;
+`;
+
+const MusicPlayStatus = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 65%;
+  margin: 0 auto 2rem auto;
+`;
+
+const MusicPlayBar = styled.div`
+  display: flex;
+  align-items: center;
+  margin-bottom: 1rem;
+  background-color: ${COLOR.white};
+  border-top-left-radius: 0.45rem;
+  border-bottom-left-radius: 0.45rem;
+  border-top-right-radius: 0.45rem;
+  border-bottom-right-radius: 0.45rem;
+
+  & div:first-of-type {
+    width: 50%;
+    height: 0.75rem;
+    background-color: ${COLOR.deepBlue};
+    border-top-left-radius: 0.45rem;
+    border-bottom-left-radius: 0.45rem;
+    border-top-right-radius: 0.45rem;
+    border-bottom-right-radius: 0.45rem;
+  }
+  & div:last-of-type {
+    width: 50%;
+    height: 0.75rem;
+    background-color: ${COLOR.white};
+    border-top-right-radius: 0.45rem;
+    border-bottom-right-radius: 0.45rem;
+  }
+`;
+
+const MusicPlayTime = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+`;
+
+const PostDetailEvent = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+
+  width: 65%;
+  margin: 0 auto;
+`;
+
+const PostIcon = styled.div`
+  display: flex;
+  flex-direction: column;
+  color: ${COLOR.white};
+
+  & img {
+    margin-bottom: 0.5rem;
+  }
+  & span {
+    font-weight: 500;
+    font-size: 0.9rem;
+    line-height: 1.4rem;
+    text-align: center;
+    letter-spacing: -0.01em;
+  }
+`;
+
+const PostPlayIcon = styled.div`
+  width: 4rem;
+  height: 4rem;
+  overflow: hidden;
+  border-radius: 50%;
+  border: 1px solid #cccccc;
+  box-sizing: content-box;
+
+  & audio {
+    margin-top: -0.75rem;
+    margin-left: -0.6rem;
+    display: block;
+  }
+`;
+
+const PostDetailContent = styled.div``;
+
+const ContenTitleContainer = styled.div`
+  background: #fbfbff;
+  box-shadow: 0.5rem 0 1.5rem rgba(135, 135, 135, 0.7);
+  border-radius: 2rem 2rem 0 0;
+  padding: 2rem 0;
+
+  position: absolute;
+  left: 0;
+  bottom: 12.5%;
+  width: 100%;
+  height: 2.5%;
+`;
+
+const ContentTitle = styled.div`
+  width: 90%;
+  margin: 0 auto;
+
+  display: flex;
+  align-items: center;
+
+  & div:nth-child(1) {
+    width: 25%;
+  }
+
+  & div:nth-of-type(2) {
+    width: 50%;
+
+    font-style: normal;
+    font-size: 1.5rem;
+    line-height: 2.2rem;
+
+    display: flex;
+    align-items: center;
+    justify-content: center;
+
+    color: ${COLOR.deepBlue};
+
+    & div:nth-of-type(1) {
+      font-weight: 700;
+    }
+    & div:nth-of-type(2) {
+      font-weight: 500;
+      margin-left: -0.5rem;
+    }
+  }
+
+  & div:nth-of-type(3) {
+    width: 25%;
+    text-align: right;
+    cursor: pointer;
+  }
+`;
+
+const ContentContainer = styled.div`
+  background: #fbfbff;
+
+  position: absolute;
+  left: 0;
+  bottom: 0;
+  width: 100%;
+  height: 13.5%;
+`;
+
+const Content = styled.div`
+  border: 0.4px solid rgba(125, 116, 220, 0.4);
+  border-radius: 1rem;
+  padding: 1rem;
+  width: 90%;
+  margin: 1.5rem auto 0 auto;
+
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.3rem;
+  line-height: 1.9rem;
+
+  color: ${COLOR.deepBlue};
+`;
--- src/components/post/detail/musicInfo/index.tsx
+++ src/components/post/detail/musicInfo/index.tsx
@@ -1,31 +1,64 @@
+import { COLOR } from '@/constants/color';
+import styled from '@emotion/styled';
 import { Music } from '../types';
 
-function MusicInfo({ musicName, musicUrl, thumbnailUrl, singer, genre }: Music) {
+function MusicInfo({ musicName, thumbnailUrl, singer }: Music) {
   return (
-    <div
-      style={{
-        width: '400px',
-        height: '250px',
-        backgroundColor: 'yellow',
-        marginBottom: '10px',
-      }}
-    >
-      <div>노래 제목 : {musicName}</div>
-      <div>
-        <audio src={musicUrl} controls loop></audio>
-      </div>
-      <div>장르 : {genre.genreName}</div>
-      <div>가수 : {singer}</div>
-      <img
-        src={thumbnailUrl}
-        alt='썸네일 이미지'
-        style={{
-          width: '100px',
-          height: '100px',
-        }}
-      />
-    </div>
+    <MusicInfoContainer>
+      <MusicItem>
+        <MusicTitle>노래 제목 : {musicName}</MusicTitle>
+        <MusicSinger>가수 : {singer}</MusicSinger>
+        <MusicThumbnail src={thumbnailUrl} />
+      </MusicItem>
+    </MusicInfoContainer>
   );
 }
 
 export default MusicInfo;
+
+const MusicInfoContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  width: 50%;
+  margin: 8rem auto 0 auto;
+`;
+
+const MusicItem = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+`;
+
+const MusicTitle = styled.div`
+  font-style: normal;
+  font-weight: 700;
+  font-size: 1.5rem;
+  line-height: 2.2rem;
+  margin-bottom: 0.5rem;
+
+  color: ${COLOR.white};
+`;
+
+const MusicSinger = styled.div`
+  font-style: normal;
+  font-weight: 500;
+  font-size: 1.2rem;
+  line-height: 1.8rem;
+  margin-bottom: 2.5rem;
+
+  text-align: center;
+
+  color: ${COLOR.white};
+`;
+
+const MusicThumbnail = styled.div<{ src: string }>`
+  background-image: url('http://www.akbobada.com/home/akbobada/archive/akbo/img/20150115102222.jpg');
+  background-position: center center;
+
+  filter: drop-shadow(0 0 1.5rem rgba(158, 158, 158, 0.25));
+  border-radius: 1rem;
+
+  width: 20rem;
+  height: 20rem;
+  margin-bottom: 4rem;
+`;
