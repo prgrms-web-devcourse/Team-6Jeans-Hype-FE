bf00daf7b952a8416bd86e39bac9c29b0afd9b30
ksh-jsx <shkim787@naver.com>
2023-02-27 00:29:33 +0900

    feat: 좋아요 및 버튼 버튼

---
+++ src/components/common/ImageButtons/Like.tsx
@@ -0,0 +1,50 @@
+import { COLOR } from '@/constants/color';
+import useLike from '@/hooks/useLike';
+import styled from '@emotion/styled';
+
+interface Props {
+  size: number;
+  initCount: number;
+  isClicked: boolean;
+  onClick?(): void;
+}
+
+function Like({ size, initCount, isClicked, onClick }: Props) {
+  const { state, onClickLike } = useLike({ initCount, isClicked });
+
+  const handleClick = () => {
+    onClickLike();
+    onClick?.();
+  };
+
+  return (
+    <LikeContainer onClick={handleClick} size={size}>
+      <img src={`/images/like-icon-${state.isClicked ? 'on' : 'off'}.svg`} />
+      <div>{state.count}</div>
+    </LikeContainer>
+  );
+}
+
+export default Like;
+
+interface StyleProp {
+  size: number;
+}
+
+const LikeContainer = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > img {
+    width: ${({ size }: StyleProp) => `${size}rem;`};
+  }
+
+  & > div {
+    text-align: center;
+    color: ${COLOR.white};
+    font-size: 1rem;
+    padding-top: 0.3rem;
+  }
+`;
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,3 +1,5 @@
+import Battle from '@/components/common/ImageButtons/Battle';
+import Like from '@/components/common/ImageButtons/like';
 import TextDivider from '@/components/common/TextDivider';
 import Link from 'next/link';
 
@@ -7,7 +9,16 @@ export default function Home() {
       <Link href='/post/searchMusics' legacyBehavior>
         <a>create</a>
       </Link>
+      {/*임시 */}
       <TextDivider text1='내가바로음잘알' text2='2023-02-21' />
+
+      <div style={{ background: 'linear-gradient(130.7deg, #A274DC -10.45%, #658DF4 122.15%);' }}>
+        <Like size={2} initCount={15} isClicked={false} onClick={() => console.log('click')} />
+        <br />
+        <Battle size={1.5} battleAbility={false} onClick={() => console.log('battle-click')} />
+        <br />
+        <Battle size={1.5} battleAbility={true} onClick={() => console.log('battle-click')} />
+      </div>
     </>
   );
 }
