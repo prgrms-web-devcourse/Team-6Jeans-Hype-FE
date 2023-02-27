d3f7d1e009f9934a596dbb7ab08ebe80663999ea
김성현 <shkim061198@gmail.com>
2023-02-27 17:55:54 +0900

    feat: bottom-nav 제작 (#56)
    * feat: bottom-nav 제작
    
    * fix: build 에러 수정
    
    * fix: 리뷰 반영

---
+++ src/components/common/BottomNav/index.tsx
@@ -0,0 +1,84 @@
+import { COLOR } from '@/constants/color';
+import styled from '@emotion/styled';
+import Link from 'next/link';
+import { useRouter } from 'next/router';
+
+interface Button {
+  src: string;
+  text: string;
+  path: string;
+}
+
+const buttonList = [
+  { src: 'main', text: '메인', path: '/' },
+  { src: 'battle', text: '대결', path: '/tmp' },
+  { src: 'share', text: '공유', path: '/post/searchMusics' },
+  { src: 'feed', text: '피드', path: '/post' },
+  { src: 'mypage', text: '마이페이지', path: '/tmp' },
+];
+
+const BottomNav = () => {
+  const router = useRouter();
+  const { pathname } = router;
+
+  return (
+    <BottomNavContainer>
+      <Buttons>
+        {buttonList.map((button: Button) => {
+          const { src, text, path } = button;
+          const isClicked = path === pathname;
+
+          return (
+            <Link href={path} legacyBehavior key={src}>
+              <Button isClicked={isClicked}>
+                <img src={`/images/bottom-nav/${src}-icon${isClicked ? '' : '-off'}.svg`} />
+                <span>{text}</span>
+              </Button>
+            </Link>
+          );
+        })}
+      </Buttons>
+    </BottomNavContainer>
+  );
+};
+
+export default BottomNav;
+
+interface styleProp {
+  isClicked: boolean;
+}
+
+const BottomNavContainer = styled.div`
+  width: 100%;
+  max-width: 76.8rem;
+  position: fixed;
+  bottom: 0;
+
+  display: flex;
+  box-shadow: 0.5rem 0 1.5rem rgba(135, 135, 135, 0.7);
+  border-radius: 2rem 2rem 0 0;
+  height: 8rem; ;
+`;
+
+const Buttons = styled.div`
+  width: calc(100% - 6rem);
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  margin: 0 auto;
+`;
+
+const Button = styled.button`
+  width: 5rem;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+
+  & > span {
+    font-size: 1rem;
+    line-height: 1.4rem;
+    color: ${({ isClicked }: styleProp) => (isClicked ? COLOR.purple : COLOR.gray)};
+    font-weight: ${({ isClicked }: styleProp) => (isClicked ? '700' : '500')};
+    margin-top: 0.6rem;
+  }
+`;
