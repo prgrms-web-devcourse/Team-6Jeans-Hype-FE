7a5b74a960eb2b2e05a6b29bff33620e9545148c
ksh-jsx <shkim787@naver.com>
2023-03-01 00:26:53 +0900

    refactor: 디자인 더 깔끔하게

--- src/hooks/useVoteResult.ts
+++ src/hooks/useVoteResult.ts
@@ -1,17 +1,36 @@
-import { useState } from 'react';
+import { useRef, useState } from 'react';
 
 const useVoteResult = () => {
   const [visible, setVisible] = useState<boolean>();
+  const refForMove = useRef<HTMLSpanElement>(null);
 
-  const onClick = () => {
+  const onVote = () => {
     setVisible((prev) => !prev);
 
     setTimeout(() => {
       setVisible((prev) => !prev);
-    }, 3000);
+    }, 1800);
   };
 
-  return { visible, onClick };
+  const onClickMusic = () => {
+    const target = refForMove.current;
+
+    if (target) {
+      const { className } = target;
+
+      target.className = `${className} active`;
+
+      setTimeout(() => {
+        onVote();
+      }, 200);
+
+      setTimeout(() => {
+        target.className = `${className}`;
+      }, 1800);
+    }
+  };
+
+  return { visible, refForMove, onVote, onClickMusic };
 };
 
 export default useVoteResult;
