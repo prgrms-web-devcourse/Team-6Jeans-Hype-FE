2cb754635c017b2e99e75d9bb309da450da87285
Park-min-hyoung <qkralswgud1324@naver.com>
2023-02-28 19:14:22 +0900

    fix: 빌드 오류 해결

--- src/pages/post/create.tsx
+++ src/pages/post/create.tsx
@@ -8,7 +8,7 @@ function Create() {
 
   return (
     <>
-      <Header title='추천 글쓰기' subButtonValue='완료' onClickSubButton={onSubmit} />
+      <Header title='추천 글쓰기' subButtonValue='완료' onClickSubButton={onSubmit} selectedColor='deepblue' />
       <PostCreate
         values={values}
         onChangeValues={onChangeValues}
