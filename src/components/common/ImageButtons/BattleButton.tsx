9afdd9d67ac5acffe4daa789e9dee82c444f8f0b
shkim787\laptop <shkim061198@gmail.com>
2023-03-02 17:54:48 +0900

    fix: 오류수정

--- src/components/common/ImageButtons/BattleButton.tsx
+++ src/components/common/ImageButtons/BattleButton.tsx
@@ -13,8 +13,8 @@ interface Props {
 function Battle({ size, color, battleAbility, onClick }: Props) {
   return (
     <BattleContainer onClick={onClick}>
-      <StyledFireIcon size={size} color={color} battleAbility={battleAbility} />
-      <Text battleAbility={battleAbility} color={color}>
+      <StyledFireIcon size={size} color={color} ability={battleAbility.toString()} />
+      <Text ability={battleAbility} color={color}>
         {battleAbility ? '대결신청' : '대결불가'}
       </Text>
     </BattleContainer>
@@ -26,7 +26,7 @@ export default Battle;
 interface StyleProp {
   size?: number;
   color?: string;
-  battleAbility: boolean;
+  ability: boolean;
 }
 
 const BattleContainer = styled.div`
@@ -37,20 +37,20 @@ const BattleContainer = styled.div`
 
 const Text = styled.div`
   text-align: center;
-  color: ${({ battleAbility, color }: StyleProp) => {
+  color: ${({ ability, color }: StyleProp) => {
     const targetColor = color === 'white' ? COLOR.white : COLOR.blue;
 
-    return battleAbility ? targetColor : COLOR.gray;
+    return ability ? targetColor : COLOR.gray;
   }};
   padding-top: 0.6rem;
 `;
 
-const StyledFireIcon = styled(FireIcon)<{ color: string; size: number; battleAbility: boolean }>`
+const StyledFireIcon = styled(FireIcon)<{ color: string; size: number; ability: string }>`
   width: ${(props) => `${props.size}rem`};
   height: ${(props) => `${props.size * 1.3}rem`};
   stroke: ${(props) => {
     const targetColor = props.color === 'white' ? COLOR.white : COLOR.blue;
 
-    return props.battleAbility ? targetColor : COLOR.gray;
+    return props.ability === 'true' ? targetColor : COLOR.gray;
   }};
 `;
