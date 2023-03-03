15237a5e9bc1864c39f54afdc2a3f179374a4b7e
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-03-03 17:46:10 +0900

    feat: 재생 버튼 공통 컴포넌트 구현 (#113)
    * feat: 재생 버튼 공통 컴포넌트 구현
    
    * fix: 빌드 오류 해결
    
    * fix: 코드 리뷰 반영

---
+++ src/components/common/MusicPlayButton/index.tsx
@@ -0,0 +1,54 @@
+import styled from '@emotion/styled';
+import { useState } from 'react';
+
+interface Props {
+  src?: string;
+}
+
+function MusicPlayButton({ src }: Props) {
+  const [isMusicPlay, setIsMusicPlay] = useState(true);
+
+  const onClickPlayButton = () => {
+    const $audioElement = document.getElementById('audio') as HTMLAudioElement;
+
+    if (isMusicPlay) $audioElement?.play();
+    else $audioElement?.pause();
+
+    setIsMusicPlay((prev) => !prev);
+  };
+
+  return (
+    <PlayIcon>
+      <audio src={src} id='audio' controls loop></audio>
+      <img
+        src={`/images/${isMusicPlay ? 'play-button' : 'google-logo'}.svg`}
+        alt='play button'
+        onClick={onClickPlayButton}
+      />
+      ;
+    </PlayIcon>
+  );
+}
+
+export default MusicPlayButton;
+
+const PlayIcon = styled.div`
+  width: 4rem;
+  height: 4rem;
+  overflow: hidden;
+  position: relative;
+
+  & audio {
+    opacity: 0;
+  }
+
+  & img {
+    position: absolute;
+    top: 50%;
+    left: 50%;
+    transform: translate(-50%, -50%);
+
+    width: 7rem;
+    height: 7rem;
+  }
+`;
