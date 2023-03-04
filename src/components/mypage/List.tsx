1d102a9a63121c08fe40cf82c8b607b3796b1f5f
dazzel3 <aakk9350@naver.com>
2023-03-05 02:11:26 +0900

    feat: List 컴포넌트 생성 후 UserContent에서 데이터 매핑

---
+++ src/components/mypage/List.tsx
@@ -0,0 +1,64 @@
+import styled from '@emotion/styled';
+import Arrow from 'public/images/arrow-right.svg';
+import { ReactNode } from 'react';
+
+import { COLOR } from '@/constants/color';
+
+interface Props {
+  title: string;
+  children: ReactNode;
+}
+
+function List({ title, children }: Props) {
+  return (
+    <Container title={title}>
+      <Header>
+        <Title>{title} 목록</Title>
+        <Button>
+          더보기
+          <Arrow />
+        </Button>
+      </Header>
+      <Content>{children}</Content>
+    </Container>
+  );
+}
+
+export default List;
+
+const Container = styled.div<{ title: string }>`
+  width: 90%;
+  height: fit-content;
+  background: ${COLOR.background};
+  box-shadow: 0px 0px 15px rgba(158, 158, 158, 0.25);
+  margin: 0 auto;
+  border-radius: 1rem;
+  margin-top: ${(props) => props.title === '대결' && '-5rem'};
+`;
+
+const Header = styled.div`
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  padding: 1.7rem 2rem;
+`;
+
+const Title = styled.h1`
+  font-weight: 700;
+  font-size: 1.4rem;
+`;
+
+const Button = styled.button`
+  color: ${COLOR.gray};
+  font-weight: 500;
+  font-size: 1rem;
+  display: flex;
+  align-items: center;
+  gap: 0.4rem;
+`;
+
+const Content = styled.div`
+  display: flex;
+  flex-direction: column;
+  gap: 1.2rem;
+`;
--- src/pages/mypage/index.tsx
+++ src/pages/mypage/index.tsx
@@ -1,10 +1,12 @@
 import BottomNav from '@/components/common/BottomNav';
+import UserContent from '@/components/mypage/UserContent';
 import UserHeader from '@/components/mypage/UserHeader';
 
 function Mypage() {
   return (
     <>
       <UserHeader />
+      <UserContent />
       <BottomNav />
     </>
   );
