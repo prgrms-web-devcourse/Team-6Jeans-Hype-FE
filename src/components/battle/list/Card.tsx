d8e8ab261391a1f792d92f88e7e26b76c59538bc
jynam <njy1007@gmail.com>
2023-03-05 02:04:58 +0900

    feat: 대결 리스트 페이지 마크업

---
+++ src/components/battle/list/Card.tsx
@@ -0,0 +1,86 @@
+import styled from '@emotion/styled';
+import Versus from 'public/images/no-background-logo.svg';
+
+import { COLOR } from '@/constants/color';
+
+import { BattleMusic } from './types';
+
+interface BattleCardProps {
+  left: BattleMusic;
+  right: BattleMusic;
+}
+
+export default function BattleCard({ left, right }: BattleCardProps) {
+  return (
+    <Container>
+      <Music>
+        <AlbumPoster src={left.albumCoverImage} alt='album-cover' />
+        <Title>{left.title}</Title>
+        <Singer>{left.singer}</Singer>
+      </Music>
+      <StyledVersus />
+      <Music>
+        <AlbumPoster src={right.albumCoverImage} alt='album-cover' />
+        <Title>{right.title}</Title>
+        <Singer>{right.singer}</Singer>
+      </Music>
+    </Container>
+  );
+}
+
+const Container = styled.div`
+  background-color: ${COLOR.white};
+  box-shadow: 0px 0px 10px rgba(226, 226, 226, 0.25);
+  border-radius: 1rem;
+  display: flex;
+  position: relative;
+  justify-content: space-between;
+  padding: 1.6rem 1.3rem;
+`;
+
+const Music = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  gap: 0.5rem;
+`;
+
+const AlbumPoster = styled.img`
+  border-radius: 10px;
+  height: 6.5rem;
+  width: 6.5rem;
+  object-fit: cover;
+`;
+
+const Title = styled.div`
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.4rem;
+  color: ${COLOR.deepBlue};
+  width: 11.8rem;
+  text-align: center;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  word-break: break-all;
+`;
+
+const StyledVersus = styled(Versus)`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+`;
+
+const Singer = styled.div`
+  font-weight: 500;
+  font-size: 1rem;
+  line-height: 1.4rem;
+  color: ${COLOR.gray};
+  width: 11.8rem;
+  text-align: center;
+  overflow: hidden;
+  white-space: nowrap;
+  text-overflow: ellipsis;
+  word-break: break-all;
+`;
