daa156bc5e43267a9554bea02465d6d9f2b9a0bb
dazzel3 <aakk9350@naver.com>
2023-03-05 19:49:43 +0900

    refactor: API url 수정

--- src/components/mypage/api.ts
+++ src/components/mypage/api.ts
@@ -2,13 +2,11 @@ import { axiosInstance } from '@/api';
 
 import { MyPostAPI } from './types';
 
-const SERVER = process.env.NEXT_PUBLIC_API_URL;
-
 export const getPostFeedLimit = async () => {
   try {
     const { data } = await axiosInstance.request<MyPostAPI>({
       method: 'GET',
-      url: `${SERVER}/members/posts`,
+      url: `/members/posts`,
       params: {
         limit: '2',
       },
