58c236a2d451873cec5a46c6fda430c20a3d419d
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-03-05 19:01:16 +0900

    feat: 추천 피드 세부 기능 구현 (#119)
    * feat: 배틀 신청 및 좋아요 아이콘 클릭 시 이벤트 버블링 금지
    
    * feat: 추천 글 최신순 정렬
    
    * feat: all 버튼 클릭 시 모든 추천 글 render
    
    * docs: BottomNav 컴포넌트 page 파일에 들어가도록 수정
    
    * refactor: DB 데이터가 최신순 정렬되어 있기 때문에 sort 제거

--- src/pages/post/index.tsx
+++ src/pages/post/index.tsx
@@ -1,7 +1,13 @@
+import BottomNav from '@/components/common/BottomNav';
 import PostList from '@/components/post';
 
 function Post() {
-  return <PostList />;
+  return (
+    <>
+      <PostList />
+      <BottomNav />;
+    </>
+  );
 }
 
 export default Post;
