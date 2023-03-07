4487aa574c0297e6f6a44c54d34c678615d63865
남주영 <njy1007@gmail.com>
2023-03-07 15:20:02 +0900

    feat: 로그인이 필요한 페이지인 경우 로그인 강제하기 (#168)

--- src/pages/post/battle/index.tsx
+++ src/pages/post/battle/index.tsx
@@ -1,7 +1,12 @@
+import AuthRequiredPage from '@/components/login/AuthRequiredPage';
 import BattleForm from '@/components/post/battle';
 
 function Battle() {
-  return <BattleForm />;
+  return (
+    <AuthRequiredPage>
+      <BattleForm />
+    </AuthRequiredPage>
+  );
 }
 
 export default Battle;
