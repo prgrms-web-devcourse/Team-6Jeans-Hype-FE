d9d755f16680018843ee65804c39d6c16c362157
Park-min-hyoung <qkralswgud1324@naver.com>
2023-03-07 15:54:36 +0900

    docs: TEMP_TOKEN 제거

--- src/components/post/battle/mybattleList/api.ts
+++ src/components/post/battle/mybattleList/api.ts
@@ -2,9 +2,6 @@ import { axiosInstance } from '@/api';
 
 import { MyBattlePosAPI } from '../types';
 
-const TEMP_TOKEN =
-  'eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI2IiwiaWF0IjoxNjc3NDg1MTUwLCJleHAiOjE2NzgzNDkxNTB9.1UtakWRXOkrN-IGZ7V7fWh0YhC4WzBS6M31FxTnPceKLW-IqvD8sTVlQIDEDfmbqxDdqqWnOVH4i0i0k1KuYlg';
-
 export const getMyBattleListData = async (genre: string) => {
   try {
     const { data } = await axiosInstance.request<MyBattlePosAPI>({
