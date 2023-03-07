46de53dd3848a3679fd093a409bde34a55a034f1
Dayeon Won <50357236+dazzel3@users.noreply.github.com>
2023-03-07 22:19:13 +0900

    chore: 대결 상세 페이지 pause 아이콘 및 스타일 수정 (#178)
    * chore: play, pause icon 추가
    
    * refactor: pause 아이콘 추가 및 태그 구조 변경
    
    * chore: 추천 상세 스타일 수정
    
    * chore: 추천 상세 textarea -> p 태그로 변경 및 스타일 수정
    
    * fix: username 최대글자 인 경우 UI 수정
    
    * fix: 타입 에러 수정

--- src/components/common/MusicPlayButton/index.tsx
+++ src/components/common/MusicPlayButton/index.tsx
@@ -8,7 +8,7 @@ interface Props {
 function MusicPlayButton({ src }: Props) {
   const [isMusicPlay, setIsMusicPlay] = useState(true);
 
-  const onClickPlayButton = (e: React.MouseEvent<HTMLImageElement>) => {
+  const onClickPlayButton = (e: React.MouseEvent<HTMLButtonElement>) => {
     e.preventDefault();
 
     const $audioElement = document.getElementById(`audio${src}`) as HTMLAudioElement;
@@ -21,13 +21,10 @@ function MusicPlayButton({ src }: Props) {
 
   return (
     <PlayIcon>
-      <audio src={src} id={`audio${src}`} controls loop></audio>
-      <img
-        src={`/images/${isMusicPlay ? 'play-button' : 'google-logo'}.svg`}
-        alt='play button'
-        onClick={(e) => onClickPlayButton(e)}
-      />
-      ;
+      <Audio src={src} id={`audio${src}`} controls loop />
+      <Button onClick={(e) => onClickPlayButton(e)}>
+        <Image src={`/images/${isMusicPlay ? 'play-button' : 'pause-button'}.svg`} alt='play button' />
+      </Button>
     </PlayIcon>
   );
 }
@@ -35,22 +32,26 @@ function MusicPlayButton({ src }: Props) {
 export default MusicPlayButton;
 
 const PlayIcon = styled.div`
-  width: 4rem;
-  height: 4rem;
+  width: 5rem;
+  height: 5rem;
+  border-radius: 50%;
   overflow: hidden;
   position: relative;
+`;
 
-  & audio {
-    opacity: 0;
-  }
+const Audio = styled.audio`
+  opacity: 0;
+`;
 
-  & img {
-    position: absolute;
-    top: 50%;
-    left: 50%;
-    transform: translate(-50%, -50%);
+const Button = styled.button`
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  cursor: pointer;
+`;
 
-    width: 7rem;
-    height: 7rem;
-  }
+const Image = styled.img`
+  height: 5rem;
+  filter: drop-shadow(0px 0px 10px rgba(43, 43, 43, 0.25));
 `;
--- src/components/post/detail/musicInfo/index.tsx
+++ src/components/post/detail/musicInfo/index.tsx
@@ -1,16 +1,16 @@
+import styled from '@emotion/styled';
+
 import AlbumPoster from '@/components/common/AlbumPoster';
 import { COLOR } from '@/constants/color';
-import styled from '@emotion/styled';
+
 import { Music } from '../types';
 
 function MusicInfo({ title, albumCoverUrl, singer }: Music) {
   return (
     <Container>
-      <Wrap>
-        <Title>{title}</Title>
-        <Singer>{singer}</Singer>
-        <AlbumPoster lazy={true} size={20} src={albumCoverUrl} />
-      </Wrap>
+      <Title>{title}</Title>
+      <Singer>{singer}</Singer>
+      <AlbumPoster lazy={true} size={20} src={albumCoverUrl} />
     </Container>
   );
 }
@@ -20,35 +20,31 @@ export default MusicInfo;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
-  width: 50%;
-  margin: 8rem auto 0 auto;
+  align-items: center;
+  margin: 6rem 0;
+  color: ${COLOR.white};
 `;
 
-const Wrap = styled.div`
-  display: flex;
-  flex-direction: column;
-  align-items: center;
-  margin-bottom: 4rem;
-`;
+// const Wrapper = styled.div`
+//   display: flex;
+//   flex-direction: column;
+//   margin-bottom: 4rem;
+// `;
 
 const Title = styled.div`
-  font-style: normal;
+  max-width: 70%;
   font-weight: 700;
   font-size: 1.5rem;
   line-height: 2.2rem;
   margin-bottom: 0.5rem;
-
-  color: ${COLOR.white};
+  text-align: center;
 `;
 
 const Singer = styled.div`
-  font-style: normal;
+  max-width: 70%;
   font-weight: 500;
   font-size: 1.2rem;
   line-height: 1.8rem;
   margin-bottom: 2.5rem;
-
   text-align: center;
-
-  color: ${COLOR.white};
 `;
