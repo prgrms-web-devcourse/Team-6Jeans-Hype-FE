3b4c68e35a6b6686f2f9b893d43b9b25aeadb031
Park-min-hyoung <qkralswgud1324@naver.com>
2023-03-08 15:54:19 +0900

    fix: 코드 리뷰 반영

--- src/components/post/battle/mybattleList/api.ts
+++ src/components/post/battle/mybattleList/api.ts
@@ -7,9 +7,6 @@ export const getMyBattleListData = async (selectedOpponentMusicId: string) => {
     const { data } = await axiosInstance.request<MyBattlePosAPI>({
       method: 'GET',
       url: `/posts/battle/${selectedOpponentMusicId}/candidates`,
-      headers: {
-        Authorization: `Bearer eyJhbGciOiJIUzUxMiJ9.eyJzdWIiOiI4IiwiaWF0IjoxNjc4MTc0MzYzLCJleHAiOjE2NzkwMzgzNjN9.5Vt-z-oEXA2-XnrQ0pQPn8OVNAPiTL9OunZcgX1H60hJOYbLx0Zwcv3zaOs_4VXpqGOi1CT2VOIwJXHVILfjlQ`,
-      },
     });
 
     if (data.success) {
