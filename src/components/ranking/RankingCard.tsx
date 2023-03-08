3f97f65bb8c04ca1e3dc703d5b3449f58b8d685c
ksh-jsx <shkim787@naver.com>
2023-03-08 14:26:57 +0900

    feat: 디자인 적용

--- src/components/ranking/RankingCard.tsx
+++ src/components/ranking/RankingCard.tsx
@@ -14,7 +14,7 @@ function RankingCard({ user, isMyRanking }: Props) {
 
   return (
     <RankingCardContainer className={isMyRanking ? 'my' : undefined}>
-      <Rank className={isMyRanking ? 'my' : undefined}>{memberRanking}</Rank>
+      <Rank>{memberRanking}</Rank>
       <Nickname>{memberNickname}</Nickname>
       <Point>{memberPoint} Points</Point>
     </RankingCardContainer>
@@ -32,14 +32,26 @@ const RankingCardContainer = styled.div`
   padding: 1rem 0;
   align-items: center;
   position: relative;
+  cursor: pointer;
 
   &.my {
     background: rgba(118, 151, 236, 0.1);
   }
+
+  &:nth-child(odd) > div:first-of-type {
+    background: rgba(125, 116, 220, 0.29);
+  }
+
+  &:hover {
+    background: rgba(125, 116, 220, 0.1);
+    position: relative;
+    bottom: 0.1rem;
+    transition: all 0.1s ease;
+  }
 `;
 
 const Rank = styled.div`
-  background: rgba(125, 116, 220, 0.29);
+  background: rgba(118, 151, 236, 0.4);
   width: 2.4rem;
   height: 2.4rem;
   align-items: center;
@@ -49,10 +61,6 @@ const Rank = styled.div`
   font-weight: 600;
   font-size: 1.4rem;
   margin: 0 1.3rem;
-
-  &.my {
-    background: rgba(118, 151, 236, 0.4);
-  }
 `;
 
 const Nickname = styled.div`
