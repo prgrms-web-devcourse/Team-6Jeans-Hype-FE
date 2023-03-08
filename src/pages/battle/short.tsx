9e55bd37eb496fa383961b7543eeb20f8626386e
ksh-jsx <shkim787@naver.com>
2023-03-08 16:39:02 +0900

    refactor: 일부 컴포는트 페이지로 이동

--- src/pages/battle/short.tsx
+++ src/pages/battle/short.tsx
@@ -1,8 +1,11 @@
+import styled from '@emotion/styled';
 import Link from 'next/link';
 import ListIcon from 'public/images/go-to-list-icon.svg';
 
 import Select from '@/components/battle/select';
+import VoteResult from '@/components/battle/voteResult';
 import BottomNav from '@/components/common/BottomNav';
+import Genres from '@/components/common/Genres';
 import Header from '@/components/common/Header';
 import useVoteResult from '@/hooks/useVoteResult';
 
@@ -20,18 +23,27 @@ function Short() {
           </Link>
         }
       />
-      <Select
-        musicData={musicData}
-        isLoadingState={isLoadingState}
-        selectedBattle={selectedBattle}
-        position={position}
-        onClickGenre={onClickGenre}
-        onClickMusic={onClickMusic}
-        onClickSkip={onClickSkip}
-      />
+      <SelectContainer>
+        <Genres onChange={onClickGenre} shouldNeedAll />
+        <Select
+          musicData={musicData}
+          isLoadingState={isLoadingState}
+          onClickMusic={onClickMusic}
+          onClickSkip={onClickSkip}
+        />
+      </SelectContainer>
+      {selectedBattle.battleId !== -1 && selectedBattle.votedPostId !== -1 && (
+        <VoteResult battleId={selectedBattle.battleId} votedPostId={selectedBattle.votedPostId} clickSide={position} />
+      )}
       <BottomNav />
     </>
   );
 }
 
 export default Short;
+
+const SelectContainer = styled.div`
+  width: calc(100% - 4rem);
+  height: calc(100vh - 16rem);
+  padding: 0 2rem;
+`;
