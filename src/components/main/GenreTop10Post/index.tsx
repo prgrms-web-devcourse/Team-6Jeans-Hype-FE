6e041749897d2798d9962984eeabb8d022b30a24
Park-min-hyoung <qkralswgud1324@naver.com>
2023-03-08 19:27:13 +0900

    feat: swiper 기능 구현

--- src/components/main/GenreTop10Post/index.tsx
+++ src/components/main/GenreTop10Post/index.tsx
@@ -4,12 +4,13 @@ import { COLOR } from '@/constants/color';
 import styled from '@emotion/styled';
 import { useQuery } from '@tanstack/react-query';
 import { useState } from 'react';
+import { Swiper, SwiperSlide } from 'swiper/react';
 import { getGenreTop10Data } from './api';
 
 function GenreTop10Post() {
   const [genre, setGenre] = useState('');
 
-  const { data: genreTop10Post, isLoading } = useQuery(['main', 'genreTop10', genre], () => {
+  const { data: genreTop10Post } = useQuery(['main', 'genreTop10', genre], () => {
     return getGenreTop10Data(genre);
   });
 
@@ -23,17 +24,25 @@ function GenreTop10Post() {
     <Container>
       <Title>이런 곡은 어때요?</Title>
       <Genres onChange={onChange} shouldNeedAll />
-      <Posts>
+      <Swiper
+        spaceBetween={30}
+        slidesPerView={3}
+        breakpoints={{
+          768: {
+            slidesPerView: 6,
+          },
+        }}
+      >
         {genreTop10Post?.map(({ postId, music: { albumCoverUrl, title, singer } }) => (
-          <Post key={postId}>
+          <SwiperSlide key={postId}>
             <AlbumPoster lazy={true} size={12.5} src={albumCoverUrl} />
             <TitleSinger>
               <div>{title}</div>
               <div>{singer}</div>
             </TitleSinger>
-          </Post>
+          </SwiperSlide>
         ))}
-      </Posts>
+      </Swiper>
     </Container>
   );
 }
@@ -43,6 +52,12 @@ export default GenreTop10Post;
 const Container = styled.div`
   display: flex;
   flex-direction: column;
+  overflow: hidden;
+
+  & .swiper-wrapper {
+    display: -webkit-inline-box;
+    margin-top: 1rem;
+  }
 `;
 
 const Title = styled.div`
@@ -53,24 +68,6 @@ const Title = styled.div`
   color: ${COLOR.deepBlue};
 
   margin-bottom: 1rem;
-`;
-
-const Posts = styled.div`
-  display: flex;
-  overflow-x: hidden;
-  &::-webkit-scrollbar {
-    display: none;
-  }
-
-  margin-top: 1rem;
-`;
-
-const Post = styled.div`
-  display: flex;
-  flex-direction: column;
-  margin-right: 0.5rem;
-
-  width: 12.5rem;
 `;
 
 const TitleSinger = styled.div`
