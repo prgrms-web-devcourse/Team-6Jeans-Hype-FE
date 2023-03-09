1f43c1d8bdf9192930d6cac87f80350e4e7a622f
Park-min-hyoung <qkralswgud1324@naver.com>
2023-03-09 20:07:48 +0900

    fix: 모바일에서는 swiper 제거

--- src/components/main/GenreTop10Post/index.tsx
+++ src/components/main/GenreTop10Post/index.tsx
@@ -1,12 +1,11 @@
-import AlbumPoster from '@/components/common/AlbumPoster';
 import Genres from '@/components/common/Genres';
 import { COLOR } from '@/constants/color';
 import styled from '@emotion/styled';
 import { useQuery } from '@tanstack/react-query';
 import { useRouter } from 'next/router';
 import { useState } from 'react';
-import { Swiper, SwiperSlide } from 'swiper/react';
 import { getGenreTop10Data } from './api';
+import MobilePosts from './MobilePosts';
 
 function GenreTop10Post() {
   const router = useRouter();
@@ -26,71 +25,20 @@ function GenreTop10Post() {
   });
 
   return (
-    <Container>
-      <Title>이런 곡은 어때요?</Title>
+    <>
       <Genres onChange={onChange} shouldNeedAll />
-      <Swiper
-        spaceBetween={30}
-        slidesPerView={3}
-        breakpoints={{
-          768: {
-            slidesPerView: 6,
-          },
-        }}
-      >
-        {genreTop10Post?.map(({ postId, music: { albumCoverUrl, title, singer } }) => (
-          <SwiperSlide key={postId} onClick={() => navigatePostDetail(postId)}>
-            <AlbumPoster lazy={true} size={12.5} src={albumCoverUrl} />
-            <TitleSinger>
-              <div>{title}</div>
-              <div>{singer}</div>
-            </TitleSinger>
-          </SwiperSlide>
-        ))}
-      </Swiper>
-    </Container>
+      <Title>이런 곡은 어때요?</Title>
+      {genreTop10Post && <MobilePosts genreTop10Post={genreTop10Post} navigatePostDetail={navigatePostDetail} />}
+    </>
   );
 }
 
 export default GenreTop10Post;
 
-const Container = styled.div`
-  display: flex;
-  flex-direction: column;
-  overflow: hidden;
-
-  & .swiper-wrapper {
-    display: -webkit-inline-box;
-    margin-top: 1rem;
-  }
-`;
-
 const Title = styled.div`
   font-weight: 700;
   font-size: 1.6rem;
   line-height: 2.3rem;
-
   color: ${COLOR.deepBlue};
-
   margin-bottom: 1rem;
-`;
-
-const TitleSinger = styled.div`
-  margin: 1rem 0 0 0.5rem;
-
-  & div:first-of-type {
-    font-weight: 500;
-    font-size: 1rem;
-    line-height: 1.8rem;
-
-    color: ${COLOR.deepBlue};
-  }
-
-  & div:last-of-type {
-    font-weight: 500;
-    font-size: 0.9rem;
-    line-height: 1.4rem;
-
-    color: ${COLOR.gray};
-  }
 `;
