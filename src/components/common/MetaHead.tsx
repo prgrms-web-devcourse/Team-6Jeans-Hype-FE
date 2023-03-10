af3dc5237fc107eb304810c36f2e922c3faf11a1
dazzel3 <aakk9350@naver.com>
2023-03-11 01:12:04 +0900

    feat: MetaHead 컴포넌트 생성

---
+++ src/components/common/MetaHead.tsx
@@ -0,0 +1,24 @@
+import Head from 'next/head';
+
+export default function MetaHead() {
+  return (
+    <Head>
+      <meta name='viewport' content='width=device-width, initial-scale=1, user-scalable=no' />
+      <meta name='theme-color' content='#fbfbff' />
+      <meta name='apple-mobile-web-app-capable' content='yes' />
+      <meta name='apple-mobile-web-app-status-bar-style' content='#fbfbff' />
+
+      <meta name='description' content="What's your Hype Music? 너만의 노래를 찾아봐!" />
+      <meta property='og:url' content='https://h-y-p-e.netlify.app' />
+      <meta property='og:image' content='/images/og-image.svg' />
+      <meta property='og:title' content='Hype | 하입 ♫•*¨*•.¸¸♪✧' />
+      <meta property='og:description' content="What's your Hype Music? 너만의 노래를 찾아봐!" />
+
+      <meta name='twitter:card' content='summary_large_image' />
+      <meta name='twitter:title' content='Hype | 하입 ♫•*¨*•.¸¸♪✧' />
+      <meta name='twitter:description' content="What's your Hype Music? 너만의 노래를 찾아봐!" />
+
+      <title>Hype | 하입 ♫•*¨*•.¸¸♪✧</title>
+    </Head>
+  );
+}
