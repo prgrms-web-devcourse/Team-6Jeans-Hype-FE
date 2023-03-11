82a070cf221fb130671a653ba7c7f754f906f4a4
jynam <njy1007@gmail.com>
2023-03-11 15:04:46 +0900

    fix: 바뀐 네이밍 적용

--- src/pages/battle/detail.tsx
+++ src/pages/battle/detail.tsx
@@ -4,7 +4,7 @@ import { useRouter } from 'next/router';
 import ListIcon from 'public/images/go-to-list-icon.svg';
 import { useState } from 'react';
 
-import DetailComponent from '@/components/battle/Detail/Battle';
+import DetailComponent from '@/components/battle/detail/Battle';
 import { useGetBattle } from '@/components/battle/select/hooks/useGetBattle';
 import { SelectedBattle } from '@/components/battle/types';
 import VoteResult from '@/components/battle/voteResult';
