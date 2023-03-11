c3d394a278bd47f12d58f457b385a0d6362a71e1
Park-min-hyoung <60873508+Park-min-hyoung@users.noreply.github.com>
2023-03-11 15:36:03 +0900

    feat: 장르별 좋아요 순으로 정렬 된 추천 글 버그 해결 및 기능 추가 (#234)
    * fix: 모바일에서는 swiper 제거
    
    * feat: desktop에서는 swiper
    
    * refactor: component pages로 이동
    
    * docs: 필요 없는 파일제거 및 소스코드 정리
    
    * fix: 모바일에서 스크롤 안되는 문제 해결

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,15 +1,37 @@
 import styled from '@emotion/styled';
+import { useQuery } from '@tanstack/react-query';
 import Link from 'next/link';
+import { useRouter } from 'next/router';
 import ArrowRight from 'public/images/arrow-right.svg';
 import Logo from 'public/images/letter-logo.svg';
+import { useState } from 'react';
 
 import BottomNav from '@/components/common/BottomNav';
-import GenreTop10Post from '@/components/main/GenreTop10Post';
+import Genres from '@/components/common/Genres';
+import { getGenreTop10Data } from '@/components/main/GenreTop10Post/api';
+import DeskTopPosts from '@/components/main/GenreTop10Post/DeskTopPosts';
+import MobilePosts from '@/components/main/GenreTop10Post/MobilePosts';
 import RandomBattle from '@/components/main/RandomBattle';
 import Ranking from '@/components/ranking';
 import { COLOR } from '@/constants/color';
+import { useCheckMobile } from '@/hooks/useCheckMobile';
 
 export default function Home() {
+  const router = useRouter();
+  const { mobile } = useCheckMobile();
+
+  const [genre, setGenre] = useState('');
+
+  const onChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const selectedGenre = e.target.value;
+
+    selectedGenre === 'ALL' ? setGenre('') : setGenre(selectedGenre);
+  };
+  const navigatePostDetail = (postId: number) => router.push(`/post/detail?postId=${postId}`);
+
+  const { data: genreTop10Post } = useQuery(['main', 'genreTop10', genre], () => {
+    return getGenreTop10Data(genre);
+  });
   const PROGRESS_BATTLE_DUMMY = {
     challengedAlbumCoverImage:
       'https://is2-ssl.mzstatic.com/image/thumb/Music115/v4/a9/6c/89/a96c893b-e5bd-2c55-897c-7604201c3335/cover-.jpg/100x100bb.jpg',
@@ -21,8 +43,13 @@ export default function Home() {
       <StyledLogo />
       <Label>진행 중인 대결</Label>
       <StyledRandomBattle {...PROGRESS_BATTLE_DUMMY} />
-      <Label>이런 곡은 어때요?</Label>
-      <GenreTop10Post />
+      <LikeGenrePost>
+        <Label>이런 곡은 어때요?</Label>
+        <Genres onChange={onChange} shouldNeedAll />
+        {mobile
+          ? genreTop10Post && <MobilePosts genreTop10Post={genreTop10Post} navigatePostDetail={navigatePostDetail} />
+          : genreTop10Post && <DeskTopPosts genreTop10Post={genreTop10Post} navigatePostDetail={navigatePostDetail} />}
+      </LikeGenrePost>
       <RankingLabels>
         <Label>오늘의 TOP 5</Label>
         <Link href='/ranking'>
@@ -68,6 +95,13 @@ const RankingLabels = styled.div`
   width: 100%;
   display: flex;
   justify-content: space-between;
+`;
+
+const LikeGenrePost = styled.div`
+  width: 100%;
+  display: flex;
+  flex-direction: column;
+  padding-bottom: 9.5rem;
 `;
 
 const SeeMore = styled.span`
