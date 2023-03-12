291b70760864f5a399622c6024061627c7278b57
dazzel3 <aakk9350@naver.com>
2023-03-13 08:28:51 +0900

    chore: NoContent 위치 수정

--- src/components/battle/detail/Battle/index.tsx
+++ src/components/battle/detail/Battle/index.tsx
@@ -119,11 +119,11 @@ const NoContentWrapper = styled.div`
   display: flex;
   flex-direction: column;
   align-items: center;
-  position: relative;
+  position: fixed;
   top: 50%;
   left: 50%;
   transform: translate(-50%, -50%);
-  gap: 0.5rem;
+  gap: 1.5rem;
 `;
 
 const Skip = styled.div`
