ef00ddb055373568731a474f7841c7584d9aff05
ksh-jsx <shkim787@naver.com>
2023-03-13 16:29:41 +0900

    chore: 누락된 디자인 추가

--- src/components/battle/detail/Battle/style.ts
+++ src/components/battle/detail/Battle/style.ts
@@ -3,6 +3,7 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
+  position: relative;
 `;
 
 export const BattleMusicWrapper = styled.div`
