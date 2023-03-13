20a6b42fdbe4e051a2e1d5a6518db246215ee578
jynam <njy1007@gmail.com>
2023-03-13 18:24:25 +0900

    chore: 로그인 안 한 상태로 마이페이지 버튼 누를 시 로그인 모달 뜨도록

--- src/components/common/BottomNav.tsx
+++ src/components/common/BottomNav.tsx
@@ -52,7 +52,8 @@ const BottomNav = () => {
     {
       src: (isClicked: boolean) => (isClicked ? <MypageOnIcon /> : <MypageIcon />),
       text: '마이페이지',
-      paths: ['/profile'],
+      paths: [isLoggedIn ? '/profile' : ''],
+      onClick: () => isLoggedIn || openAuthRequiredModal(),
     },
   ];
 
