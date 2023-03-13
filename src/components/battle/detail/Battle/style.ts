bbd15909f9a59861944326d009b912bba5e69eaa
ksh-jsx <shkim787@naver.com>
2023-03-13 16:13:29 +0900

    chore: 필요없는 css 삭제

--- src/components/battle/detail/Battle/style.ts
+++ src/components/battle/detail/Battle/style.ts
@@ -3,7 +3,6 @@ import styled from '@emotion/styled';
 export const Container = styled.div`
   display: flex;
   flex-direction: column;
-  position: relative;
 `;
 
 export const BattleMusicWrapper = styled.div`
