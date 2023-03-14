28235f2e0516e48e186858fdab6e8600fafa1a7f
jynam <njy1007@gmail.com>
2023-03-14 15:12:43 +0900

    chore: GA 측정 위해 GTM 연동

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -4,8 +4,12 @@ import { Global } from '@emotion/react';
 import { QueryClient, QueryClientProvider } from '@tanstack/react-query';
 import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
 import type { AppProps } from 'next/app';
+import { useRouter } from 'next/router';
+import { useEffect } from 'react';
 import { RecoilRoot } from 'recoil';
 
+import * as gtm from '@/components/googleTagManager/gtm';
+import GoogleTagManagerScript from '@/components/googleTagManager/Script';
 import AuthRequiredModal from '@/components/login/AuthRequiredModal';
 import { global } from '@/styles/global';
 
@@ -14,8 +18,18 @@ const queryClient = new QueryClient({
 });
 
 export default function App({ Component, pageProps }: AppProps) {
+  const router = useRouter();
+
+  useEffect(() => {
+    router.events.on('routeChangeComplete', gtm.pageview);
+    return () => {
+      router.events.off('routeChangeComplete', gtm.pageview);
+    };
+  }, [router.events]);
+
   return (
     <QueryClientProvider client={queryClient}>
+      <GoogleTagManagerScript />
       <ReactQueryDevtools initialIsOpen={false} />
       <RecoilRoot>
         <Global styles={global} />
